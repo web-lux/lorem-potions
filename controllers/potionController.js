082e1696f80be9a8b430f1223ba9d1dde248635c
web-lux <web-lux@outlook.fr>
2024-01-15 14:14:53 +0100

    rewrite potionController to async

--- controllers/potionController.js
+++ controllers/potionController.js
@@ -3,17 +3,23 @@ const Categories = require("../models/categories");
 const { body, validationResult } = require("express-validator");
 const { decode } = require("html-entities");
 
-const potion_index = (req, res) => {
-    Potions.find()
-        .populate("category")
-        .then(data => res.render("potionIndex", { title: "Potions", potions: data, decode: decode }))
-        .catch(err => console.log(err));
+const potion_index = async (req, res) => {
+    try {
+        const potions = await Potions.find().populate("category").exec();
+        res.render("potionIndex", { title: "Potions", potions: potions, decode: decode });
+    } catch (err) {
+        console.error(err);
+    }
 };
 
-const potion_create_get = (req, res) => {
-    Categories.find().sort({ name: 1 }).exec()
-        .then(categories => res.render("potionForm", { title: "Create potion", formAction: "/potions/create", potion: null, errors: null, categories: categories, decode: decode }));
-    };
+const potion_create_get = async (req, res) => {
+    try {
+        const categories = await Categories.find().sort({ name: 1 }).exec();
+        res.render("potionForm", { title: "Create potion", formAction: "/potions/create", potion: null, errors: null, categories: categories, decode: decode });
+    } catch (err) {
+        console.error(err);
+    }
+};
 
 const potion_create_post = [
     body("name", "Name should be between 1 and 64 characters").trim().isLength({ min: 1, max: 64 }).escape(),
@@ -48,44 +54,40 @@ const potion_create_post = [
     }
 ]
 
-const potion_details = (req, res) => {
-    Potions.findOne({ _id: req.params.id })
-        .populate("category")
-        .exec()
-        .then(data => {
-            if (data === null) {
-                res.redirect("/404");
-            } else {
-                res.render("potionDetails", { title: data.name, potion: data, decode: decode });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.redirect("404");
-        });
+const potion_details = async (req, res) => {
+    try {
+        const potion = await Potions.findOne({ _id: req.params.id }).populate("category").exec();
+        if (potion === null) {
+            res.redirect("/404");
+        } else {
+            res.render("potionDetails", { title: potion.name, potion: potion, decode: decode });
+        }
+    } catch (err) {
+        console.error(err);
+    }
 };
 
-const potion_delete = (req, res) => {
-    Potions.findOneAndDelete({ _id: req.params.id })
-        .then(data => res.json({ redirect: "/potions" }))
-        .catch(err => console.log(err));
+const potion_delete = async (req, res) => {
+    try {
+        await Potions.findOneAndDelete({ _id: req.params.id });
+        res.json({ redirect: "/potions" });
+    } catch (error) {
+        console.error(err);
+    }
 };
 
 const potion_modify_get = async (req, res) => {
-    const categories = await Categories.find().sort({ name: 1 }).exec();
-
-    Potions.findOne({ _id: req.params.id })
-    .exec()
-    .then(data => {
-        if (data === null) {
+    try {
+        const categories = await Categories.find().sort({ name: 1 }).exec();
+        const potion = await Potions.findOne({ _id: req.params.id }).exec();
+        if (potion === null) {
             res.redirect("/404");
         } else {
-            res.render("potionForm", { title: "Modify potion", formAction: `/potions/${req.params.id}/modify`, potion: data, errors: null, categories: categories, decode: decode });
+            res.render("potionForm", { title: "Modify potion", formAction: `/potions/${req.params.id}/modify`, potion: potion, errors: null, categories: categories, decode: decode });
+        }
+    } catch (err) {
+        console.error(err)
     }
-    })
-    .catch(err => {
-        res.redirect("404");
-    });
 };
 
 const potion_modify_post = [
